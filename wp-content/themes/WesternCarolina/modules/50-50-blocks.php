85627f5826f05ca26342d796a0dc25c08c5dba49
Scott Baker <scott@bakerdesign.co>
2022-10-18 23:37:33 -0400

    add eyebrow

--- wp-content/themes/WesternCarolina/modules/50-50-blocks.php
+++ wp-content/themes/WesternCarolina/modules/50-50-blocks.php
@@ -1,33 +1,35 @@
 <?php
-// Collage Module
 global $field;
-// var_dump();
-// $cardColor = 'purple';
-// $header = $field['header'];
-// $copy = $field['copy'];
-// $images = $field['images'];
-// $buttonColor = 
-$blocks = $field['5050_blocks'];
-?>
+  
+
+
+$blocks = $field['5050_blocks']; ?>
 <div class="fifty-blocks"><?php 
 $count = 0;
 foreach($blocks as $block) : 
-// var_dump();
-if($count % 2 === 0) {
-  $class = 'even';
-} else {
-  $class = 'odd';
-}
-?>
+  
+  if($count % 2 === 0) {
+    $class = 'even';
+  } else {
+    $class = 'odd';
+} ?>
 
 <div class="block <?php echo $class ?>">
-  
+
   <figure>
     <img src="<?php echo $block['image']['sizes']['fifty'] ?>" />
   </figure>
   <div>
-    <h3 class="display"><?php echo $block['header']?></h3>
-    <p><?php echo $block['copy']; ?></p>
+    <?php if( $block['eyebrow_text']) : ?>
+      <h4 class="eyebrow sans-serif"><?php echo $block['eyebrow_text'] ?></h4>
+    <?php endif; ?>
+    <?php if( $block['header']) : ?>
+      <h3 class="display"><?php echo $block['header']?></h3>
+    <?php endif; ?>
+    <?php if($block['copy'])  :?>
+      <p><?php echo $block['copy']; ?></p>
+    <?php endif; ?>
+      
     <?php  if(($block['button'])){ ?> 
     <a class="button " href="<?php echo $block['button']['url'] ?>"><?php echo  $block['button']['title'] ?></a>
     <?php }?>
