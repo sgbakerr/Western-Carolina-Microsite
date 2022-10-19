43b0ab4220a8c31c76826131842885143dc2442d
Scott Baker <scott@bakerdesign.co>
2022-10-19 12:39:17 -0400

    form styled

--- wp-content/themes/WesternCarolina/functions/enqueue.php
+++ wp-content/themes/WesternCarolina/functions/enqueue.php
@@ -14,8 +14,10 @@ Enqueue Scripts
 function register_styles_scripts() {
     wp_register_style( 'styles', get_stylesheet_directory_uri() . '/assets/css/app.css', null, null, 'all' );
     wp_register_style( 'tiny_css', 'https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css', null, null, 'all' );
+    wp_register_script( 'formEmbed', 'https://ugadmissions.wcu.edu/register/?id=8b4a5647-ecae-4488-a556-1483f8b22113&output=embed&div=form_embed', null, null, true );
     // wp_register_style( 'owl_theme', get_stylesheet_directory_uri() . '/assets/css/owl.theme.default.min.css', null, null, 'all' );
-    wp_register_script( 'app', get_stylesheet_directory_uri() . '/assets/js/app.min.js', array( 'jquery', 'tinyslider'), null, true );
+    wp_register_script( 'app', get_stylesheet_directory_uri() . '/assets/js/app.min.js', array( 'jquery', 'tinyslider', 'formEmbed'), null, true );
+    
     wp_register_script( 'tinyslider', 'https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js', array( 'jquery' ), null, true );
     wp_register_script( 'modernizr', get_stylesheet_directory_uri() . '/assets/js/modernizr.js', null, null, false );
     if ( ! is_admin() ) {
@@ -23,6 +25,7 @@ function register_styles_scripts() {
         wp_enqueue_style( 'tiny_css' );
 			
         wp_enqueue_script( 'modernizr' );
+        wp_enqueue_script( 'formEmbed' );
 			
         wp_enqueue_script( 'tinyslider' );
 				wp_enqueue_script( 'app' );
