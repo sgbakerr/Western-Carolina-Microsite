60208ff0184d698eafd225812a49751b6f4ee237
Scott Baker <scott@bakerdesign.co>
2022-10-19 09:42:08 -0400

    added form to site

--- wp-content/themes/WesternCarolina/modules/form-banner.php
+++ wp-content/themes/WesternCarolina/modules/form-banner.php
@@ -9,6 +9,8 @@ global $field;
   </figure>
   <div class="card form">
     <?php echo $field['form'] ?>
+    <div id="form_8b4a5647-ecae-4488-a556-1483f8b22113">Loading...</div><script>/*<![CDATA[*/var script = document.createElement('script'); script.async = 1; script.src = 'https://ugadmissions.wcu.edu/register/?id=8b4a5647-ecae-4488-a556-1483f8b22113&output=embed&div=form_8b4a5647-ecae-4488-a556-1483f8b22113' + ((location.search.length > 1) ? '&' + location.search.substring(1) : ''); var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s);/*]]>*/</script>
+
   </div>
 
 </div>
